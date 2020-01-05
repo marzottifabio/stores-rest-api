54406ab85d4348e8141dcec14bea7b75bc7a5127
Fabio Marzotti <marzottifabio@gmail.com>
2020-01-05 20:19:39 +0100

    changed db url for connection to  postgres

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
-
+                                     # if environment system variable DATABASE_URL is not available, it will use sqllite:... 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
